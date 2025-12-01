24f43266f7865a721a4efd915b2197e9f23322f8
Kamrul Hasan <116941825+kamrul35728@users.noreply.github.com>
2025-12-01 17:07:42 +0600

    Add imports for SearchOverlay component
    import all components for SearchOverlay.jsx file

--- client/src/components/Layout/SearchOverlay.jsx
+++ client/src/components/Layout/SearchOverlay.jsx
@@ -1,1 +1,5 @@
-
+import { useState } from "react";
+import { X, Search } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toggleSearchBar } from "../../store/slices/popupSlice";
