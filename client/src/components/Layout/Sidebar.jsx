7ae545ecba878fbd1b33e710dc5bf42278f26abc
Kamrul Hasan <116941825+kamrul35728@users.noreply.github.com>
2025-12-01 18:22:05 +0600

    Import necessary components and hooks in Sidebar

--- client/src/components/Layout/Sidebar.jsx
+++ client/src/components/Layout/Sidebar.jsx
@@ -1,1 +1,14 @@
+import {
+  X,
+  Home,
+  Package,
+  Info,
+  HelpCircle,
+  ShoppingCart,
+  List,
+  Phone,
+} from "lucide-react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleSidebar } from "../../store/slices/popupSlice";
 
