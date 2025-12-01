0fce1ff876e40cd0e468a3d98c96418842dad883
Kamrul Hasan <116941825+kamrul35728@users.noreply.github.com>
2025-12-01 19:16:19 +0600

    Add imports for product detail page functionality
    import all components

--- client/src/pages/ProductDetail.jsx
+++ client/src/pages/ProductDetail.jsx
@@ -1,1 +1,19 @@
-
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  Star,
+  ShoppingCart,
+  Heart,
+  Share2,
+  Plus,
+  Minus,
+  Loader,
+  CircleDollarSign,
+} from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import ReviewsContainer from "../components/Products/ReviewsContainer";
+import { addToCart } from "../store/slices/cartSlice";
+import { fetchProductDetails } from "../store/slices/productSlice";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { formatCurrency } from "../lib/currency";
