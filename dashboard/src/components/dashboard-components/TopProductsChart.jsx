33d197417596ec679b9d67baba88a222c40a6e35
Kamrul Hasan <116941825+kamrul35728@users.noreply.github.com>
2025-12-03 01:57:25 +0600

    introduce TopProductsChart UI component

--- dashboard/src/components/dashboard-components/TopProductsChart.jsx
+++ dashboard/src/components/dashboard-components/TopProductsChart.jsx
@@ -41,7 +41,57 @@ const TopProductsChart = () => {
     return null;
   };
 
-  
+  return (
+    <>
+      <div className="bg-white p-4 rounded-xl shadow-md">
+        <h3 className="font-semibold mb-2">Top Selling Products</h3>
+        <div className="relative">
+          <ResponsiveContainer width="100%" height={250}>
+            <BarChart
+              layout="vertical"
+              data={topSellingProducts.slice(0, 3)}
+              margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+              barSize={50}
+            >
+              <XAxis type="number" />
+              <YAxis
+                dataKey="image"
+                type="category"
+                tick={<CustomYAxisTick />}
+                width={50}
+              />
+              <Tooltip
+                content={<CustomTooltip />}
+                wrapperStyle={{ pointerEvents: "auto" }}
+              />
+              <Bar
+                dataKey="total_sold"
+                radius={[4, 4, 4, 4]}
+                isAnimationActive={false}
+                onMouseEnter={() => {}}
+                onMouseLeave={() => {}}
+              >
+                {topSellingProducts.slice(0, 3).map((entry, index) => {
+                  return (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={
+                        index === 0
+                          ? "#3b82f6"
+                          : index === 1
+                          ? "#10b981"
+                          : "#f59e0b"
+                      }
+                    />
+                  );
+                })}
+              </Bar>
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default TopProductsChart;
