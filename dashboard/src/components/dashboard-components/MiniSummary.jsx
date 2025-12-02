24b01e704a4f8881eff8985534cf1ea364c36096
Kamrul Hasan <116941825+kamrul35728@users.noreply.github.com>
2025-12-03 00:24:53 +0600

    add dashboard summary cards

--- dashboard/src/components/dashboard-components/MiniSummary.jsx
+++ dashboard/src/components/dashboard-components/MiniSummary.jsx
@@ -33,8 +33,42 @@ const MiniSummary = () => {
     (acc,count)=> acc+count,
     0
   );
+  const summary=[
+    {
+      text:"Total Sales this Month",
+      subText: `This month's sales: BDT ${currentMonthSales}`,
+      icon:<Wallet className="text-green-600"/>
+    },
+    {
+      text:"Total Orders Placed",
+      subText: `Total orders placed: ${totalOrders}`,
+      icon: <PackageCheck className="text-green-600"/>
+    },
+    {
+      text:"Top  Selling Product",
+      subText: topSeller
+        ? `Best Seller: ${topSeller.name} (${topSeller.total_sold} sold)`
+        : "No sales data yet",
+      icon: <TrendingUp className="text emerald-600"/>
+    },
+    {
+      text:"Low Stock Alerts",
+      subText: lowStockSubText,
+      icon:<AlertTriangle className="text-red-600"/>
+    },
+    {
+      text:"Revenue Growth Rate",
+      subText: `Revenue ${revenueGrowth.includes("+") ? "up" : "down"} by ${revenueGrowth} compares to last month`,
+      icon:<BarChart4 className="text-purple-600"/>
+    },
+    {
+      text:"New Customers This Month",
+      subText: `New Customers joined: ${newUsersThisMonth}`,
+      icon: <UserPlus className="text-yellow-600"/>
+    },
+  ]
 
-  
+
 };
 
 export default MiniSummary;
