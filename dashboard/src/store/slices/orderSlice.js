05a93d160e7fe5fdc35686f58a39a9924c385214
2021331100 <138570295+2021331100@users.noreply.github.com>
2025-12-03 09:10:28 -0800

    Update orderSlice.js just fetch all orders

--- dashboard/src/store/slices/orderSlice.js
+++ dashboard/src/store/slices/orderSlice.js
@@ -1,1 +1,18 @@
 
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { axiosInstance } from "../../lib/axios";
+import { toast } from "react-toastify";
+
+export const fetchAllOrders = createAsyncThunk(
+  "orders/fetchAll",
+  async (_, thunkAPI) => {
+    try {
+      const { data } = await axiosInstance.get("/order/admin/getall");
+      return data.orders;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(
+        error.response?.data?.message || "Failed to fetch orders."
+      );
+    }
+  }
+);
